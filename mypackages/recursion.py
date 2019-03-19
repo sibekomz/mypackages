9bd57a2f4e6b150e98486ac488759fcb25b59d19
mzwandile sibeko <sbekomz@gmail.com>
2019-03-19 14:26:34 +0200

    update codes

--- mypackages/recursion.py
+++ mypackages/recursion.py
@@ -3,8 +3,8 @@ def sum_array(array):
     list =0
     for i in array:
         total= total + i
+        '''Return sum of all items in array'''
         return list
-  '''Return sum of all items in array'''
 
 def fibonacci(n):
     if n == 0:
@@ -12,25 +12,19 @@ def fibonacci(n):
     elif n == 1:
         return 1
     else:
-        return fibonacci(n-1) + fibonacci(n-2)
-
-        for n in fibonacci(n):
-            print (str(n))
-
     '''Return nth term in fibonacci sequence'''
+    return fibonacci(n-1) + fibonacci(n-2)
 
 def factorial(n):
     if n == 0:
         return 1
     elif n == 1:
         return 1
-
     else:
+        '''Return n!'''
         return n * factorial(n - 1)
-
-    '''Return n!'''
 
 def reverse(word):
     rev = word[::-1]
+    '''Return word in reverse'''
     return rev
-'''Return word in reverse'''
--- mypackages/sorting.py
+++ mypackages/sorting.py
@@ -1,16 +1,11 @@
-def bubble_sort(items):
-
-    exchanges = True
-    i = len(items)-1
-    while i > 0 and exchanges:
-        exchanges = False
+def bubbleSort(arr):
+    n = len(arr)
+    for i in range(n-1,0,-1):
         for j in range(i):
-            if items[j]>items[j+1]:
-                exchanges = True
-                items[j], items[j+1] = items[j+1], items[j]
-                i -= 1
-                print(items)
-    '''Return array of items, sorted in ascending order'''            
+            if arr[j] > arr[j+1] :
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
+    '''Return array of items, sorted in ascending order'''
 
 def merge_sort(items):
     if len(items) < 2:return items
@@ -23,10 +18,9 @@ def merge_sort(items):
     while (len(y) > 0) and (len(z) > 0):
             if y[0] > z[0]:result.append(z.pop(0))
             else:result.append(y.pop(0))
-
     result.extend(y+z)
-    return result
     '''Return array of items, sorted in ascending order'''
+    return result
 
 def quick_sort(items):
     def quick_sort(l):
@@ -36,7 +30,7 @@ def quick_sort(items):
     first = [x for x in l if x == start]
     smaller = quick_sort([x for x in l if x < start])
     larger = quick_sort([x for x in l if x > start])
-    return smaller + first + larger
 
     '''Return array o
     gnbfv dcswf items, sorted in ascending order'''
+    return smaller + first + larger
